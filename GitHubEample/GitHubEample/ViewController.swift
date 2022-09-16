a0c790eb5f0cc3b81fd376499e046a3ce395ca3f
Rinat <rinat.khaeritdinov@icloud.com>
2022-09-16 16:32:59 +0300

    add label

--- GitHubEample/GitHubEample/ViewController.swift
+++ GitHubEample/GitHubEample/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let firstLabel:UILabel = {
+        let label = UILabel(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        label.backgroundColor = .red
+        label.text = "Hello"
+        label.textAlignment = .center
+        label.bounds = .init(x: 90, y: 90, width: 90, height: 90)
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(firstLabel)
     }
 
 
